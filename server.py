7205e0321afffc115ae870ea3a3b4d5c14e2ef46
Jordan Hennemann <jhennemannit@gmail.com>
2026-07-14 20:07:24 -0500

    Add current workspace state

---
+++ server.py
@@ -0,0 +1,37 @@
+import litserve as ls
+import argparse
+from fastapi.middleware.cors import CORSMiddleware
+from model import Llama3
+
+
+class Llama3VisionAPI(ls.LitAPI):
+    def setup(self, device):
+        self.model = Llama3(device)
+
+    def decode_request(self, request):
+        return self.model.apply_chat_template(request.messages)
+
+    def predict(self, inputs, context):
+        yield self.model(inputs)
+
+    def encode_response(self, outputs):
+        for output in outputs:
+            yield {"role": "assistant", "content": self.model.decode_tokens(output)}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Start the llama3 vision server")
+    parser.add_argument("--port", type=int, default=10111, help="Port number to run the server on")
+    args = parser.parse_args()
+    api = Llama3VisionAPI()
+    
+    middlewares = [
+        (CORSMiddleware, 
+         {"allow_origins": ["*"], 
+          "allow_credentials": True, 
+          "allow_methods": ["*"], 
+          "allow_headers": ["*"]})
+    ]
+ 
+    server = ls.LitServer(api, accelerator="auto", middlewares=middlewares, spec=ls.OpenAISpec())
+    server.run(port=args.port)
